085f20363fa0cbab8ff8444c5a35701528366a19
gokberkozcicek <gokberkozcicek@gmail.com>
2024-05-05 17:47:18 +0300

    create material class #1

---
+++ NETFEA/NETFEA/Material/FeMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETFEA
+{
+    public abstract class FeMaterial
+    {
+        public int Id {  get; set; }
+        public FeMaterial() { }
+        public virtual MaterialType MaterialType { get; set; }
+    }
+    public enum MaterialType
+    {
+        Isotropic,
+        Orthotropic
+    }
+}
---
+++ NETFEA/NETFEA/Material/FeMaterialCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETFEA
+{
+    public class FeMaterialCollection:Dictionary<int,FeMaterial>
+    {
+        public new void Add(int key,FeMaterial value) {  base.Add(key, value); }
+    }
+}
